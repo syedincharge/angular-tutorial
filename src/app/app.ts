885ed77ae696e1b816a87df3a7fec71c8dcb379f
syedincharge <57073374+syedincharge@users.noreply.github.com>
2026-06-07 23:02:00 -0500

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login ],
+  imports: [RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  name = "Angular Tutorial";
-  isAdmin = false;
-  price = 199.99;
-  count = 5;
-  title = signal("Angular Tutorial Signal");
-  getUser() {
-    return "Syed Rzivi";
+
+  
   }
   //protected readonly title = signal('angular-tutorial');
 }
