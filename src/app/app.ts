c39c30c8ce3b0dbfcf779d9d6ade7e3de4d44095
syedincharge <57073374+syedincharge@users.noreply.github.com>
2026-06-07 22:52:14 -0500

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,61 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  name = "Angular Tutorial";
-  isAdmin = false;
-  price = 199.99;
-  count = 5;
-  title = signal("Angular Tutorial Signal");
-  getUser() {
-    return "Syed Rzivi";
+   //value = 10;
+ //value = "Imtiaz";
+  
+  //name: number | string = "Syed Imtiaz Rizvi";
+  name: number | string = 20;
+  age:number = 35;
+  isValid: boolean = true;
+  
+
+  handleClick() {
+
+    let value;
+    value = 10;
+    value = "Syed";
+
+    console.log(value);
+
   }
-  //protected readonly title = signal('angular-tutorial');
+
+  sum(a: number, b: number) {
+    console.log(a + b);
+     return a + b;
+  }
+
+  clickHandle() {
+    console.log('Button Clicked');
+  }
+
+  onTyping(event: any) {
+    console.log("Typed : ",event.target.value);
+  }
+
+  onKeyUp(event: any) {
+    console.log('Key Up Event', event.key);
+  }
+
+  onHover() {
+    console.log('Hover Event');
+  }
+  onLeave() {
+    console.log('Mouse Leave');
+  }
+
+  onBlur() {
+    console.log('Blur Event');
+  }
+
+    onFocus() {
+    console.log('Focus Event');
+  }
 }
