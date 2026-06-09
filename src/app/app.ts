50003f94e8845d2ee57fc8dfb35f20483f970187
syedincharge <57073374+syedincharge@users.noreply.github.com>
2026-06-09 12:06:41 -0500

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,54 +8,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-   //value = 10;
- //value = "Imtiaz";
-  
-  //name: number | string = "Syed Imtiaz Rizvi";
-  name: number | string = 20;
-  age:number = 35;
-  isValid: boolean = true;
-  
+  isLoggedIn = true;
+  marks = 65;
+  showBox = true;
 
-  handleClick() {
-
-    let value;
-    value = 10;
-    value = "Syed";
-
-    console.log(value);
-
+  toggleBox() {
+    this.showBox = !this.showBox;
   }
 
-  sum(a: number, b: number) {
-    console.log(a + b);
-     return a + b;
+  age = 0;
+
+  updateAge(val: string) {
+    this.age = Number(val);
+    console.log(val);
   }
 
-  clickHandle() {
-    console.log('Button Clicked');
-  }
-
-  onTyping(event: any) {
-    console.log("Typed : ",event.target.value);
-  }
-
-  onKeyUp(event: any) {
-    console.log('Key Up Event', event.key);
-  }
-
-  onHover() {
-    console.log('Hover Event');
-  }
-  onLeave() {
-    console.log('Mouse Leave');
-  }
-
-  onBlur() {
-    console.log('Blur Event');
-  }
-
-    onFocus() {
-    console.log('Focus Event');
-  }
 }
+
