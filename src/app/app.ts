afb0eb3ae287badf212bbb800b01a233c11414bb
syedincharge <57073374+syedincharge@users.noreply.github.com>
2026-06-09 07:37:50 -0500

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,13 +5,37 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
-  
-  }
-  //protected readonly title = signal('angular-tutorial');
+  count: number = 0;
+
+  // handleAdd(){
+  //   this.count++;
+  // }
+  // handleMinus() {
+  //   if(this.count > 0){
+  //       this.count--;
+  //   }
+   
+  // }
+  // handleReset() {
+  //   this.count = 0;
+  // }
+
+
+  handleCounter(value: string) {
+    if (value === 'plus') {
+      this.count++;
+    }
+    else if (value === 'minus' && this.count > 0) { 
+      this.count--;
+    }
+    else if (value === 'reset') {
+      this.count = 0;
+    }  
+  } 
 }
